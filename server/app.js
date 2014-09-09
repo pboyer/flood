b7477b7db8e55176f41f46663e1e5b65dee63b84
pboyer <peter.b.boyer@gmail.com>
2014-09-08 21:52:15 -0400

    Allow starting both SSL and normal http

--- server/app.js
+++ server/app.js
@@ -232,16 +232,14 @@ if ( fs.existsSync( keyfn ) || fs.existsSync( crtfn ) ){
   var crt = fs.readFileSync(crtfn, 'utf8');
   var cred = { key: key, cert: crt };
 
-  // https.createServer(options, app).listen(443);
   https.createServer(cred, app).listen(443, function() {
     console.log("✔ Secure Express server listening on port %d in %s mode", 443, app.get('env'));
   });
 
-} else {
-  app.listen( app.get('port'), function() {
-    console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
-  });
 }
 
+app.listen( app.get('port'), function() {
+  console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+});
 
 module.exports = app;
