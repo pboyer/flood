3e0704620371e1d9d4ba445405f886f41b73459c
pboyer <peter.b.boyer@gmail.com>
2014-09-19 10:25:14 -0500

    Open custom node workspace on double click of node

--- app/scripts/views/NodeViews/CustomNode.js
+++ app/scripts/views/NodeViews/CustomNode.js
@@ -6,6 +6,13 @@ define(['underscore', 'jquery', 'ThreeCSGNodeView'], function(_, $, ThreeCSGNode
 
     getCustomContents: function() {
 
+      var that = this;
+
+      // open the parent workspace on double click
+      this.$el.bind('dblclick', function(){
+        this.model.workspace.app.openWorkspace( this.model.get('type').functionId );
+      }.bind(this) );
+
       var js = this.model.toJSON() ;
       if (!js.extra.script) js.extra.script = this.model.get('type').script;
 
